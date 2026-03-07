f1ee6435d316a23826c07bac7bdef6b68e3fb4d7
acybeeshal <acybeeshal@gmail.com>
2026-03-08 01:15:58 +0545

    adding

---
+++ script.js
@@ -0,0 +1,9 @@
+// Simple welcome message
+window.onload = () => {
+  console.log("Welcome to Bhuwan Baral's Portfolio");
+};
+
+
+function goToInterests() {
+  window.location.href = "interests.html";
+}
